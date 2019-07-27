f2c22642966d56ca5b3478bdfe0ece035efaadaf
waylon ye <waylon.ye@oocl.com>
2019-07-27 13:01:00 +0800

    [refactor] home page to layout component

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Layout from './components/Layout';
 
 Vue.use(Router);
 
@@ -10,19 +10,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
-      meta: {
-        id: 'home'
-      }
+      redirect: '/home'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/home',
+      component: Layout,
+      meta: { id: 'home' },
+      children: [
+        {
+          path: '/',
+          component: () => import('./views/Home.vue')
+        },
+        {
+          path: '/about',
+          meta: { id: 'about' },
+          component: () => import('./views/About.vue')
+        }
+      ]
     }
   ]
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,5 @@
 <template>
-  <div class="main-container">
-    <sidebar class="side-bar"/>
-    <div class="main-view">
-      <Header />
-      <AppMain />
-    </div>
+  <div class="about">
+    <h1>This is an home page</h1>
   </div>
 </template>
-
-<script>
-import Header from '@/components/Header';
-import Sidebar from '@/components/Sidebar';
-import AppMain from '@/components/AppMain';
-
-export default {
-  name: 'home',
-  components: {
-    Header,
-    Sidebar,
-    AppMain
-  }
-};
-</script>
-
-<style lang="scss" scoped>
-.main-container {
-  display: flex;
-  min-height: 100vh;
-}
-.side-bar {
-  border: none;
-  min-width: 210px;
-  max-width: 210px;
-}
-.main-view {
-  min-height: 100%;
-  width: calc(100% - 210px);
-  transition: margin-left .28s;
-}
-</style>
