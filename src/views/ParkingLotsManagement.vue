e56762ce58619d7f8e66bd4b452379d10d717180
AhGang <474494594@qq.com>
2019-07-27 17:32:49 +0800

    [{feat}] {create a pagination view about parking lots }

--- src/views/ParkingLotsManagement.vue
+++ src/views/ParkingLotsManagement.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <ParkingLotsTable></ParkingLotsTable>
+    <div>
+      <ParkingLotsTable></ParkingLotsTable>
+      <ParkingLotsPagination></ParkingLotsPagination>
+    </div>
   </div>
 </template>
 
 <script>
 import ParkingLotsTable from "../components/ParkingLotsTable.vue";
+import ParkingLotsPagination from "../components/ParkingLotsPagination.vue";
 export default {
   name: "parking-lots-management",
   components: {
-    ParkingLotsTable
+    ParkingLotsTable,
+    ParkingLotsPagination
   }
 };
 </script>
