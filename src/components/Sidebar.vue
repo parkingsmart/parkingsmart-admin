40326fcd6af1ee69ccc9acf1380b36f7f4913bf9
waylon ye <waylon.ye@oocl.com>
2019-07-27 12:33:04 +0800

    [feat] add sidebar at the home page

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-menu
+    router
+    :default-active="$route.path"
+    text-color="#fff"
+    background-color="#1F2D3D"
+    active-text-color="#ffd04b"
+  >
+    <div v-for="(router, idx) in routers" :key="idx">
+      <el-submenu v-if="router.type === 'group'" :index="idx + ''">
+        <template slot="title">
+          <span>{{router.title}}</span>
+        </template>
+        <el-menu-item
+          v-for="item in router.items"
+          :key="item.path"
+          class="menu-item"
+          :index="item.path"
+          :route="{path: item.path}"
+        >
+          <font-awesome-icon :icon="item.navIcon" />
+          <span slot="title">{{item.id}}</span>
+        </el-menu-item>
+      </el-submenu>
+
+      <el-menu-item v-else class="menu-item" :index="router.path" :route="{path: router.path}">
+        <font-awesome-icon :icon="router.navIcon" />
+        <span slot="title">{{router.id}}</span>
+      </el-menu-item>
+    </div>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      routers: [
+        { id: "user", navIcon: "tachometer-alt", path: "/user" }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.menu-item svg {
+  padding: 0 15px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,38 @@
 <template>
-  <div class="home">
-    <Header />
+  <div class="main-container">
+    <sidebar class="side-bar"/>
+    <div class="main-view">
+      <Header />
+    </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header';
+import Sidebar from '@/components/Sidebar';
 
 export default {
   name: 'home',
   components: {
-    Header
+    Header,
+    Sidebar
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.main-container {
+  display: flex;
+  min-height: 100vh;
+}
+.side-bar {
+  border: none;
+  min-width: 210px;
+  max-width: 210px;
+}
+.main-view {
+  min-height: 100%;
+  width: calc(100% - 210px);
+  transition: margin-left .28s;
+}
+</style>
