f9b939b41f88e6512b93aa8bbf1248f9b0991152
waylon ye <waylon.ye@oocl.com>
2019-07-27 12:18:44 +0800

    [chore] use font-awesome

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
+
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
