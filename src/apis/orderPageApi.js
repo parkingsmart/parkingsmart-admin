e4091d3de2e8b01941f2c646ae0609a17f299228
montatLeast <314099319@qq.com>
2019-07-28 17:29:56 +0800

    [feat] add order page pagination

--- src/apis/orderPageApi.js
+++ src/apis/orderPageApi.js
@@ -1,7 +1,11 @@
 import axios from './base';
 
-const getAllOrders = () => {
-    return axios.get('http://localhost:8080/orders');
+const getAllOrders = (page) => {
+    return axios.get('http://localhost:8082/orders',{
+        params: {
+        page: page
+      }
+    });
 };
 
 export default {
--- src/views/OrderPage.vue
+++ src/views/OrderPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <el-table
-      :data="orders.slice((currpage - 1) * pagesize, currpage * pagesize)"
+      :data="orders"
       border
       :highlight-current-row="true"
       style="width: 100%"
@@ -11,17 +11,16 @@
       :cell-style="{'padding-left':'10px'}"
     >
       <el-table-column prop="id" label="id"></el-table-column>
-      <el-table-column prop="carNumebr" label="车牌号">
-      </el-table-column>
+      <el-table-column prop="carNumebr" label="车牌号"></el-table-column>
       <el-table-column prop="type" label="类型">
-              <template slot-scope="{row}">
-        <span>{{ getType(row.type) }}</span>
-      </template>
+        <template slot-scope="{row}">
+          <span>{{ getType(row.type) }}</span>
+        </template>
       </el-table-column>
-      <el-table-column prop="status" label="状态">        
-      <template slot-scope="{row}">
-        <span>{{ getStatus(row.status) }}</span>
-      </template>
+      <el-table-column prop="status" label="状态">
+        <template slot-scope="{row}">
+          <span>{{ getStatus(row.status) }}</span>
+        </template>
       </el-table-column>
       <el-table-column fixed="right" label="操作">
         <template slot-scope="{row}">
@@ -54,22 +53,35 @@ export default {
     };
   },
   async created() {
-    const response = await RequestHandler.invoke(OrderPageApi.getAllOrders())
-      .loading()
-      .exec();
-    this.orders = response.pageOrders;
-    this.number = response.AllOrdersNum;
-    console.log(response);
+    this.requestOrder(1,true);
   },
   methods: {
     handleCurrentChange(cpage) {
       this.currpage = cpage;
+      this.requestOrder(cpage,false);
     },
-    getType(type){
-      return type === 1 ? '取车' : '存车';
+    getType(type) {
+      return type === 1 ? "取车" : "存车";
     },
-    getStatus(status){
-      return status === 2 ? '已完成' : status === 1 ? '存取中' : '无人处理';
+    getStatus(status) {
+      return status === 2 ? "已完成" : status === 1 ? "存取中" : "无人处理";
+    },
+    async requestOrder(page, isFirst) {
+      let response;
+      if (isFirst) {
+        response = await RequestHandler.invoke(OrderPageApi.getAllOrders())
+          .msg()
+          .loading()
+          .exec();
+      } else {
+        response = await RequestHandler.invoke(OrderPageApi.getAllOrders(page))
+          .msg()
+          .loading()
+          .exec();
+      }
+      this.orders = response.pageOrders;
+      this.number = response.AllOrdersNum;
+      console.log(response);
     }
   }
 };
