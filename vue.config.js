a637431f0471fe2bef542a7e0b3856cdbda166bf
waylon ye <waylon.ye@oocl.com>
2019-07-29 02:40:36 +0800

    [chore] fix cors issue

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,13 @@ module.exports = {
     overlay: {
       warnings: true,
       errors: true
+    },
+    proxy: {
+      '/api': {
+          target: 'http://127.0.0.1/',
+          ws: true,
+          changeOrigin: true
+      },
     }
   }
 };
